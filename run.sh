7272c86689ac1d8d3e43e9908620ef5cc6d7fb42
Fernando Fernandes <fernando@backslash-f.com>
2019-06-14 14:29:53 +0200

    Silence terminal output
     - It's just too much.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=iohid_wrap.dylib /Applications/RemotePlay.app/Contents/MacOS/RemotePlay
+DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=iohid_wrap.dylib /Applications/RemotePlay.app/Contents/MacOS/RemotePlay >/dev/null 2>&1
